611d4572c5628e9fd8c308165e7af6e772767eb3
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-01-30 22:27:37 +0200

    исправление ошибок

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React, {useEffect } from 'react';
 import PropTypes from 'prop-types'
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.HendelKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.HendelKeyDown);
-  }
-
-  HendelKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.isOpen();
-    }
-  };
-  CloseModalBackdrop = e => {
+export const Modal=({isOpen, largeImageURL})=>  {
+ 
+  useEffect(()=>{
+    const hendelKeyDown = e => {
+      if (e.code === 'Escape') {
+        isOpen();
+      }
+    };
+    window.addEventListener('keydown', hendelKeyDown)
+return ()=>{ window.removeEventListener('keydown', hendelKeyDown);}},[isOpen])
+ 
+  const closeModalBackdrop = e => {
     if (e.currentTarget === e.target) {
-      this.props.isOpen();
+      isOpen();
     }
   };
-  render() {
-    return (
-      <div onClick={this.CloseModalBackdrop} className="Overlay">
-        <div className="Modal">
-          <img src={this.props.largeImageURL} alt="" />
-        </div>
+  return (
+    <div onClick={closeModalBackdrop} className="Overlay">
+      <div className="Modal">
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 Modal.propTypes={
   isOpen:PropTypes.func,
